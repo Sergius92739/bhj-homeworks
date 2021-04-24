dd11dd1b30c907523d39edb3cc2a62b05f72d5b2
Sergius92739 <sergem279@gmail.com>
2021-04-24 20:32:25 +0300

    first commit popups

---
+++ element-search/popups/task.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const cache = {};
+cache.modal = document.getElementsByClassName("modal");
+const active = cache.modal[0].className = "modal modal_active";
+const close = Array.from(cache.modal).forEach((item) => {
+    item.querySelector(".modal__close").onclick = () => {
+        item.className = "modal";
+    }
+})
+const showSuccess = cache.modal[0].querySelector(".show-success").onclick = () => cache.modal[1].className = "modal modal_active";
+
+
