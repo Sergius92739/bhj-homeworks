dcc2d61b945343fd8e71b1b841020063599f4f42
Sergius92739 <sergem279@gmail.com>
2021-04-25 18:54:38 +0300

    v1 menu

---
+++ element-search/menu/task.js
@@ -0,0 +1,14 @@
+const menuItem = document.querySelectorAll(".menu__link");
+const menuSub = document.querySelectorAll(".menu.menu_sub");
+menuItem.forEach((elem) => {
+  elem.onclick = () => {
+    menuSub.forEach((elem) => elem.className = "menu menu_sub");
+    if (elem.nextElementSibling) {
+      elem.nextElementSibling.classList.toggle("menu_active");
+      return false;
+    }
+  }
+})
+
+
+
