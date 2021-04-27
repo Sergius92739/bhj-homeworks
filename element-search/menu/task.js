0e03c110a23d30b4c2c8275ad2bb3cf919b6102f
Sergius92739 <sergem279@gmail.com>
2021-04-27 23:35:02 +0300

    2 commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,11 +4,17 @@ const menuLink = document.querySelectorAll(".menu__link");
 const menuSub = document.querySelectorAll(".menu_sub");
 
 for (let i = 0; i < menuLink.length; i++) {
+
   menuLink[i].onclick = function () {
-    menuSub.forEach(elem => elem.classList.remove("menu_active"));
-    if (menuLink[i].nextElementSibling) {
-      menuLink[i].nextElementSibling.classList.toggle("menu_active")
+
+    let nextElem = menuLink[i].nextElementSibling;
+
+    if (nextElem && nextElem.classList.contains("menu_active")) {
+      nextElem.classList.remove("menu_active")
+    } else if (nextElem) {
+      menuSub.forEach(elem => elem.classList.remove("menu_active"));
+      nextElem.classList.toggle("menu_active");
+      return false;
     }
-    return false;
   }
 }
