434739528289d1088032c7a8cdf5a22d389947c5
Sergius92739 <sergem279@gmail.com>
2021-04-27 22:03:22 +0300

    2 commit popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -6,8 +6,12 @@ const active = cache.modal[0].className = "modal modal_active";
 const close = Array.from(cache.modal).forEach((item) => {
     item.querySelector(".modal__close").onclick = () => {
         item.className = "modal";
+
     }
 })
-const showSuccess = cache.modal[0].querySelector(".show-success").onclick = () => cache.modal[1].className = "modal modal_active";
+const showSuccess = cache.modal[0].querySelector(".show-success").onclick = () => {
+    cache.modal[1].className = "modal modal_active";
+    cache.modal[0].className = "modal";
+}
 
 
